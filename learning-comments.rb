241c6fbb7093aabd53aacb545610269d67613166
Ethan Gustafson <ethanmgustafson@gmail.com>
2021-05-27 19:44:35 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,10 @@
+# name = "Byron"
+name = "Luca"
+ 
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in
+the store."
+ 
+#=> We're sorry, but per health inspector's rules, Luca is not allowed in the store.
+
+
+It's common for developers to test two code paths (in effect, doing a selection statement's work by-hand!) by "commenting-out" and "commenting-back-in" code.
